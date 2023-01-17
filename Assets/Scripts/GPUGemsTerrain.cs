ae5cb465e690c85d053c0f5ed0dbd37b6f11da3a
TiredOfEverything <113528677+TiredOfEverything@users.noreply.github.com>
2023-01-16 20:35:27 -0500

    Made noise more varied per octave

--- Assets/Scripts/GPUGemsTerrain.cs
+++ Assets/Scripts/GPUGemsTerrain.cs
@@ -8,82 +8,36 @@ using UnityEngine;
 using UnityEngine.Rendering;
 public class GPUGemsTerrain : MonoBehaviour {
     public enum NoiseType { Perlin, Voronoi, Simplex, Value, Worley }
-    public NoiseType noiseType;
-    [Range (8, 32)] public int resolution = 32;
+
+    [Range (8, 64)] public int resolution = 32;
     public Vector3Int size = Vector3Int.one;
     public Material material;
     public MARCHING_MODE mode = MARCHING_MODE.CUBES;
-    public int seed = 0;
     public float baseHeight, hardFloor = -13, hardFloor2 = 3, hardFloor3 = 40, lacunarity = 2.0f, persistence = 0.5f;
+    public float redistribution = 1f;
+    public List<Octave> octaves;
+    [System.Serializable]
+    public class Octave {
+        public int seed = 0;
+        public NoiseType noiseType;
+        public float frequency = 1, amplitude = 1, terrace = 0.5f;
+        public FractalNoise fractal;
+    }
     public bool smoothNormals = false;
     public bool drawNormals = false;
     private List<GameObject> meshes = new List<GameObject> ();
     private NormalRenderer normalRenderer;
-    public List<Octave> octaves;
     public float3 offset;
-    public float riverFrequency, riverAmplitude ,riverFalloff;
+    FractalNoise densityFractal;
 
-    [System.Serializable]
-    public class Octave {
-        public float frequency = 1, amplitude = 1;
-    }
     void Start () {
         //GenerateMesh ();
     }
-
-    public float Density (int x, int y, int z, int width, int height, int depth, FractalNoise fractal) {
-        float u = x / (width - 1.0f);
-        float v = y / (height - 1.0f);
-        float w = z / (depth - 1.0f);
-        float3 ws = new float3 (u * size.x, (v + baseHeight) * size.y, w * size.z);
-        ws += new float3 (offset);
-
-        float density = -ws.y;
-        for (int i = 0; i < octaves.Count; i++) {
-            float frequency = octaves[i].frequency * Mathf.Pow (lacunarity, i);
-            float amplitude = octaves[i].amplitude * Mathf.Pow (persistence, i);
-            density += fractal.Sample3D (ws * frequency) * amplitude;
-        }
-        float hard_floor_y = hardFloor;
-        density += math.saturate ((hard_floor_y - ws.y) * hardFloor2) * hardFloor3;
-
-        return density;
-    }
-    public float RiverDensity (int x, int y, int z, int width, int height, int depth, FractalNoise fractal) {
-        float u = x / (width - 1.0f);
-        float v = y / (height - 1.0f);
-        float w = z / (depth - 1.0f);
-        float n = fractal.Sample3D (u * riverFrequency, v * riverFrequency, w * riverFrequency) * riverAmplitude;
-        float d = 1 - (Mathf.Abs (n - 0.5f) * 2);
-        d = Mathf.Pow (d, riverFalloff);
-        return d;
-    }
-
     public void GenerateMesh () {
-        INoise perlin = new PerlinNoise (seed, 1.0f);
-        INoise voronoi = new VoronoiNoise (seed, 1.0f);
-        INoise simplex = new SimplexNoise (seed, 1.0f);
-        INoise value = new ValueNoise (seed, 1.0f);
-        INoise worley = new WorleyNoise (seed, 1.0f, 0, 1);
-        INoise selectedInoise = null;
-        switch (noiseType) {
-            case NoiseType.Perlin:
-                selectedInoise = perlin;
-                break;
-            case NoiseType.Voronoi:
-                selectedInoise = voronoi;
-                break;
-            case NoiseType.Simplex:
-                selectedInoise = simplex;
-                break;
-            case NoiseType.Value:
-                selectedInoise = value;
-                break;
-            case NoiseType.Worley:
-                selectedInoise = worley;
-                break;
+        foreach (var o in octaves) {
+            INoise selectedInoise = GenerateNewNoise (o.noiseType, o.seed);
+            o.fractal = new FractalNoise (selectedInoise, 1, 1.0f);
         }
-        FractalNoise fractal = new FractalNoise (selectedInoise, 1, 1.0f);
         //Set the mode used to create the mesh.
         //Cubes is faster and creates less verts, tetrahedrons is slower and creates more verts but better represents the mesh surface.
         Marching marching = null;
@@ -100,14 +54,12 @@ public class GPUGemsTerrain : MonoBehaviour {
         int height = resolution * size.y;
         int depth = resolution * size.z;
         var voxels = new VoxelArray (width, height, depth);
-
         //Fill voxels with values. Im using perlin noise but any method to create voxels will work.
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 for (int z = 0; z < depth; z++) {
-                    float density = Density (x, y, z, width, height, depth, fractal);
-                    float riverDensity = RiverDensity (x, y, z, width, height, depth, fractal);
-                    voxels[x, y, z] = Mathf.Min (density, riverDensity);
+                    float density = Density (x, y, z, width, height, depth);
+                    voxels[x, y, z] = density;
                 }
             }
         }
@@ -139,6 +91,49 @@ public class GPUGemsTerrain : MonoBehaviour {
         }
         var position = new Vector3 (-width / 2f, -height / 2f, -depth / 2f);
         CreateMesh32 (verts, normals, indices, position);
+    }
+    public float Density (int x, int y, int z, int width, int height, int depth) {
+        float u = x / (width - 1.0f);
+        float v = y / (height - 1.0f);
+        float w = z / (depth - 1.0f);
+        float3 ws = new float3 (u * size.x, (v + baseHeight) * size.y, w * size.z);
+        ws += new float3 (offset);
+        float density = -ws.y;
+        for (int i = 0; i < octaves.Count; i++) {
+            densityFractal = octaves[i].fractal;
+            float frequency = octaves[i].frequency * Mathf.Pow (lacunarity, i);
+            float amplitude = octaves[i].amplitude * Mathf.Pow (persistence, i);
+            density += Mathf.Round ((densityFractal.Sample3D (ws * frequency) * amplitude) * octaves[i].terrace) / octaves[i].terrace;
+        }
+        float hard_floor_y = hardFloor;
+        density += math.saturate ((hard_floor_y - ws.y) * hardFloor2) * hardFloor3;
+        return density;
+    }
+    public INoise GenerateNewNoise (NoiseType noiseType, int seed) {
+        INoise selectedInoise = null;
+        switch (noiseType) {
+            case NoiseType.Perlin:
+                INoise perlin = new PerlinNoise (seed, 1.0f);
+                selectedInoise = perlin;
+                break;
+            case NoiseType.Voronoi:
+                INoise voronoi = new VoronoiNoise (seed, 1.0f);
+                selectedInoise = voronoi;
+                break;
+            case NoiseType.Simplex:
+                INoise simplex = new SimplexNoise (seed, 1.0f);
+                selectedInoise = simplex;
+                break;
+            case NoiseType.Value:
+                INoise value = new ValueNoise (seed, 1.0f);
+                selectedInoise = value;
+                break;
+            case NoiseType.Worley:
+                INoise worley = new WorleyNoise (seed, 1.0f, 0, 1);
+                selectedInoise = worley;
+                break;
+        }
+        return selectedInoise;
     }
     private void CreateMesh32 (List<Vector3> verts, List<Vector3> normals, List<int> indices, Vector3 position) {
         Mesh mesh = new Mesh ();
